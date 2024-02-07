81333d312235993f75e742dfccc512645ed9a0a9
zecola <xyk816@gmail.com>
2024-02-07 16:24:47 +0800

    编写业务逻辑

--- src/main/java/com/zecola/cleme/controller/EmployeeController.java
+++ src/main/java/com/zecola/cleme/controller/EmployeeController.java
@@ -36,22 +36,35 @@ public class EmployeeController {
         password = DigestUtils.md5DigestAsHex(password.getBytes());
 //      2、根据页面提交的用户名username查询数据库
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
         Employee emp = employeeService.getOne(queryWrapper);
 //      3、如果没有查询到则返回登录失败结果
-        if(emp == null)
+        if (emp == null)
             return R.error("登录失败");
 //      4、密码比对，如果不一致则返回登录失败结果
-        if(!emp.getPassword().equals(password)){
+        if (!emp.getPassword().equals(password)) {
             return R.error("登录失败,密码错误");
         }
 //      5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
-        if(emp.getStatus() == 0){
+        if (emp.getStatus() == 0) {
             return R.error("员工账号已禁用");
         }
 //      6、登录成功，将员工id存入Session并返回登录成功结果
-        request.getSession().setAttribute("empId",emp.getId());
+        request.getSession().setAttribute("employee", emp.getId());
         return R.success(emp);
+    }
+
+
+    /**
+     * 员工退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public R<String>logout(HttpServletRequest request){
+        request.getSession().removeAttribute("employee");
+        return R.success("用户退出成功");
+
     }
 
 
