e4ab936a2114bf0cfdb4e726fcc888ac9aeb0336
zecola <xyk816@gmail.com>
2024-03-23 15:40:51 +0800

    用户端验证码逻辑实现

--- src/main/java/com/zecola/cleme/controller/UserController.java
+++ src/main/java/com/zecola/cleme/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.zecola.cleme.controller;
 
+import com.zecola.cleme.common.R;
+import com.zecola.cleme.pojo.User;
 import com.zecola.cleme.service.UserService;
+import com.zecola.cleme.utils.MailUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @Slf4j
@@ -13,4 +21,32 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private MailUtils mailUtils;
+
+
+    /**
+     * 发送验证码
+     * @param user
+     * @param session
+     * @return
+     * @throws MessagingException
+     */
+    @PostMapping("/sendMsg")
+    public R<String> sendMsg(@RequestBody User user, HttpSession session) throws MessagingException {
+        String phone = user.getPhone();
+        if (!phone.isEmpty()) {
+            //随机生成一个验证码
+            String code = MailUtils.achieveCode();
+            log.info(code);
+            //这里的phone其实就是邮箱，code是我们生成的验证码
+
+            mailUtils.sendTestMail(phone, code);
+            //验证码存session，方便后面拿出来比对
+            session.setAttribute(phone, code);
+            return R.success("验证码发送成功");
+        }
+        return R.error("验证码发送失败");
+    }
 }
--- src/main/java/com/zecola/cleme/filter/LoginCheckFilter.java
+++ src/main/java/com/zecola/cleme/filter/LoginCheckFilter.java
@@ -35,7 +35,10 @@ public class LoginCheckFilter implements Filter {
                 "/employee/login",
                 "/employee/logout",
                 "/backend/**",
-                "/front/**"
+                "/front/**",
+                //对用户登录放行
+                "/user/login",
+                "/user/sendMsg"
         };
         //2、判断本次请求是否需要处理
         boolean check = check(urls, requestURI);
@@ -47,7 +50,7 @@ public class LoginCheckFilter implements Filter {
             filterChain.doFilter(request,response);
             return;
         }
-        //4、判断登录状态，如果已登录，则直接放行
+        //4、判断管理端登录状态，如果已登录，则直接放行
         if (request.getSession().getAttribute("employee") != null) {
             log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("employee"));
 
@@ -60,6 +63,16 @@ public class LoginCheckFilter implements Filter {
             filterChain.doFilter(request,response);
             return;
         }
+
+        //5.判断用户是否登录
+        if(request.getSession().getAttribute("user") != null){
+            log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("user"));
+            Long userId = (Long)request.getSession().getAttribute("user");
+            BaseContext.setCurrentId(userId);
+            filterChain.doFilter(request,response);
+            return;
+        }
+
         log.info("用户未登录");
         //5、如果未登录则返回未登录结果,通过输出流向客户端页面响应数据
         response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
