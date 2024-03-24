1a4e56ff9499be6fb386822e8ee85faffc80d99d
zecola <xyk816@gmail.com>
2024-03-24 11:05:27 +0800

    用户端购物车功能开发

---
+++ src/main/java/com/zecola/cleme/service/ShoppingCartService.java
@@ -0,0 +1,8 @@
+package com.zecola.cleme.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zecola.cleme.pojo.ShoppingCart;
+
+public interface ShoppingCartService extends IService<ShoppingCart> {
+}
---
+++ src/main/java/com/zecola/cleme/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,11 @@
+package com.zecola.cleme.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zecola.cleme.mapper.ShoppingCartMapper;
+import com.zecola.cleme.pojo.ShoppingCart;
+import com.zecola.cleme.service.ShoppingCartService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShoppingCartServiceImpl extends ServiceImpl<ShoppingCartMapper, ShoppingCart> implements ShoppingCartService {
+}
